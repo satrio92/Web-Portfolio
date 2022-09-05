dff82676de42d584e0da2e4384cadbe50ddf611d
satrio92 <kukuhsatrio920@gmail.com>
2022-09-05 20:24:57 +0700

    add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        k2d: ['K2D']
+      }
+    },
   },
   plugins: [],
 }
